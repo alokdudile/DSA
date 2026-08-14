87f1a1b76aeff4f843766c414ac79f6992146b2a
Alok Dudile <157030144+alokdudile@users.noreply.github.com>
2026-08-15 02:19:22 +0530

    Time: 312 ms (26.78%) | Memory: 19.3 MB (19.02%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,8 @@
+# ALok
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        last = self.fib(n - 1)
+        slast = self.fib(n - 2)
+        return last + slast
